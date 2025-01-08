90e6c0cfb74f39b7b30e9bafa84b0ce25eff9558
Koushik Roy <154724597+iscordian@users.noreply.github.com>
2025-01-08 07:19:22 +0530

    Delete themeManager.js

--- themeManager.js
+++
@@ -1,5 +0,0 @@
-export function applyTheme(themeName) {
-    document.body.className = ''; // Clear all existing classes
-    if (themeName !== 'light') {
-        document.body.classList.add(`theme-${themeName}`);
-}
